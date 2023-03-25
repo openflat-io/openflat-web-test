a6b6df4a72ac6cda799a21d4290409272cba5d8d
Leooeloel <1826234293@qq.com>
2023-03-25 20:20:19 +0800

    refactor(web): update

---
+++ web/flat-web/deploy.sh
@@ -0,0 +1,19 @@
+# deploy.sh
+# stop operation when an error occurs
+set -e
+
+# build
+yarn build
+
+# go to the target folder
+cd dist
+
+# commit to repo
+git init
+git add -A
+git commit -m 'deploy'
+
+# push to the target branch
+git push -f git@github.com:openflat-io/openflat-web-test.git master:gh-pages
+
+cd -
